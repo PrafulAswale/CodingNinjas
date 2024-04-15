d46cee8b1af7a99f8ec015d2276e5483869952b0
Praful Aswale <prafulcode15@gmail.com>
2024-04-15 08:48:56 +0530

    commit all changes

---
+++ Backend/nodews/modules_node_js/index.js
@@ -0,0 +1,14 @@
+const operations = require('./operations');
+
+
+console.log(operations.add(1,2));
+console.log(operations.multiply(32,2));
+
+// function add(a,b) {
+//     return a+b;
+// }
+
+// console.log(add(2,3));
+// console.log(process.argv);
+// var args = process.argv.slice(2);
+// console.log("Adding the numbers: ", add(parseInt(args[0]), parseInt(args[1])));
